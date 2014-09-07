e7ccfdca6bcab2036e0e33356c9e6d5cd19635ac
Owen Wang <wangfowen@gmail.com>
2014-09-07 12:27:47 -0700

    got mustcontains working

--- api.js
+++ api.js
@@ -3,13 +3,40 @@ function Code(ast) {
     this.ast = ast;
   }
 
+  code.prototype.traverse = function(node, func) {
+    if (!node || typeof node.type !== "string") {
+      return;
+    }
+
+    func(node);
+
+    for (var property in node) {
+      var child = node[property];
+
+      if (Array.isArray(child)) {
+        for (var i = 0; i < child.length; i++) {
+          this.traverse(child[i], func);
+        }
+      } else {
+        this.traverse(child, func);
+      }
+    }
+  };
+
   code.prototype.mustContain = function(type) {
-    console.log(this.ast);
-    return this;
+    var contains = false;
+
+    this.traverse(this.ast, function(ast) {
+      if (ast.type === type) {
+        contains = true;
+      }
+    });
+
+    return contains;
   };
 
   code.prototype.mustNotContain = function(type) {
-    return this;
+    return !this.mustContain(type);
   };
 
   code.prototype.whichMustContain = function(type) {
@@ -21,7 +48,7 @@ function Code(ast) {
   };
 
   code.prototype.test = function() {
-    return true;
+    return Object.getOwnPropertyNames(this.ast).length !== 0;
   };
 
   return new code(ast);
